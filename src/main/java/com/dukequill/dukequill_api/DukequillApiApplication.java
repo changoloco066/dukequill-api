a374869188295891913b091833ac6bcfbeb98af9
changoloco066 <bigoteschicos@gmail.com>
2026-07-23 00:54:44 -0600

    first commit

---
+++ src/main/java/com/dukequill/dukequill_api/DukequillApiApplication.java
@@ -0,0 +1,13 @@
+package com.dukequill.dukequill_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DukequillApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DukequillApiApplication.class, args);
+	}
+
+}
